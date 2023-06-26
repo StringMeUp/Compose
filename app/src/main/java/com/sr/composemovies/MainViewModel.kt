4a73e162014f9e26055f301c0e7378c3c096b73f
Samir Ramic <ramic.samirr@gmail.com>
2023-06-26 12:55:21 +0200

    [SR-01] Scaffold with vm (lifecycle)

---
+++ app/src/main/java/com/sr/composemovies/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.sr.composemovies
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(): ViewModel() {
+}
