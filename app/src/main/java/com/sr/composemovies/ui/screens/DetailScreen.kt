4a7275e0613e5e1ba0bd986ab10e2135451d307f
Samir Ramic <ramic.samirr@gmail.com>
2023-06-28 13:08:39 +0200

    [SR-10] Default arguments

--- app/src/main/java/com/sr/composemovies/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/sr/composemovies/ui/screens/DetailScreen.kt
@@ -1,10 +1,8 @@
 package com.sr.composemovies.ui.screens
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,11 +14,11 @@ import com.sr.composemovies.MainViewModel
 
 @Composable
 @Preview(showBackground = true)
-fun DetailScreen(viewModel: MainViewModel = viewModel()) {
+fun DetailScreen(value: String? = null, viewModel: MainViewModel = viewModel()) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
-        Text(text = "This is detail screen.", color = Color.Black)
+        Text(text = "Detail screen: $value", color = Color.Black)
     }
 }
