848e3f69f29aea0e00582e10bed8f31007c49478
Samir Ramic <ramic.samirr@gmail.com>
2023-06-28 10:32:32 +0200

    [SR-09] Theming

--- app/src/main/java/com/sr/composemovies/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/sr/composemovies/ui/screens/DetailScreen.kt
@@ -1,21 +1,26 @@
 package com.sr.composemovies.ui.screens
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.sr.composemovies.MainViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sr.composemovies.MainViewModel
 
 @Composable
 @Preview(showBackground = true)
 fun DetailScreen(viewModel: MainViewModel = viewModel()) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Hello detail", color = Color.Black)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(text = "This is detail screen.", color = Color.Black)
     }
 }
