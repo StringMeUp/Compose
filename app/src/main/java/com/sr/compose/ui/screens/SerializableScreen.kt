851fa01d4b6d1985045c0c4c08eb9157f34f79b4
Samir Ramic <ramic.samirr@gmail.com>
2023-07-07 12:17:22 +0200

    [SR-22] Image

--- app/src/main/java/com/sr/compose/ui/screens/SerializableScreen.kt
+++ app/src/main/java/com/sr/compose/ui/screens/SerializableScreen.kt
@@ -1,17 +1,25 @@
 package com.sr.compose.ui.screens
 
 import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import com.sr.compose.R
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,7 +27,6 @@ import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import com.sr.compose.MainViewModel
-import com.sr.compose.R.*
 
 @Composable
 fun SerializableScreen(navController: NavController, args: MainViewModel.ComposeItem?) {
@@ -32,7 +39,7 @@ fun SerializableScreen(navController: NavController, args: MainViewModel.Compose
 @Composable
 fun SetUpSerializable(args: MainViewModel.ComposeItem? = null, onClick: () -> Unit = {}) {
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        val (title, descr, spacer) = createRefs()
+        val (title, descr, spacer, image) = createRefs()
         Text(text = args?.text ?: "Well, this didn't work honey.",
             textAlign = TextAlign.Center,
             color = Color.Black,
@@ -55,10 +62,8 @@ fun SetUpSerializable(args: MainViewModel.ComposeItem? = null, onClick: () -> Un
                 end.linkTo(parent.end)
             })
 
-        Text(text = "In this case we used Gson to be able to simply " +
-                "serialize/deserialize our custom object and pass it along. " +
-                "In comparison to safe-args/or the out-of-the-box arguments " +
-                "in the fragment navController component (graph) this is just ridiculous.",
+        Text(text = "Passing custom Serializables/Parcelables is strictly speaking against core android principles, " +
+                "and also not recommended by the Jetpack Compose team. However, it can be done.",
             textAlign = TextAlign.Center,
             color = Color.Black,
             modifier = Modifier
@@ -69,6 +74,20 @@ fun SetUpSerializable(args: MainViewModel.ComposeItem? = null, onClick: () -> Un
                 }
                 .fillMaxWidth(),
             fontSize = 24.sp)
+
+        Image(
+            painter = painterResource(id = R.drawable.mrbunny),
+            contentDescription = "Custom bunny serializable,",
+            modifier = Modifier
+                .padding(24.dp)
+                .clip(RoundedCornerShape(corner = CornerSize(8.dp)))
+                .size(200.dp)
+                .constrainAs(image) {
+                    top.linkTo(descr.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            contentScale = ContentScale.Crop)
     }
 
     Column(
@@ -82,7 +101,7 @@ fun SetUpSerializable(args: MainViewModel.ComposeItem? = null, onClick: () -> Un
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(24.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = color.s_color)),
+            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = com.sr.compose.R.color.s_color)),
             onClick = { onClick() }) {
             Text(text = "Done", color = Color.White)
         }
