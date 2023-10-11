42834fb74a9fe6a6b60c29afcabbd28e21f34e5a
Samir Ramic <ramic.samirr@gmail.com>
2023-10-11 12:07:16 +0200

    [SR-35] Constraint Layout ::fillConstraints

--- app/src/main/java/com/sr/compose/ui/screens/bottomnavscreens/Home.kt
+++ app/src/main/java/com/sr/compose/ui/screens/bottomnavscreens/Home.kt
@@ -1,34 +1,28 @@
 package com.sr.compose.ui.screens.bottomnavscreens
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -41,6 +35,8 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sr.compose.MainViewModel
 import com.sr.compose.R
@@ -85,67 +81,92 @@ private fun MovieCard(movie: Movie) {
             .padding(5.dp)
             .clip(shape = RoundedCornerShape(8.dp))
     ) {
-        Row {
-            Box(
+
+        ConstraintLayout(
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth()
+                .wrapContentHeight(),
+        ) {
+            val (poster, titleText, infoText, toggle, detailText) = createRefs()
+
+            Image(
+                painterResource(R.drawable.all_night),
+                contentScale = ContentScale.Crop,
+                contentDescription = "poster",
                 modifier = Modifier
+                    .constrainAs(poster) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                    }
                     .size(100.dp)
                     .padding(6.dp)
                     .clip(CircleShape)
-            ) {
-                Image(
-                    painterResource(R.drawable.all_night),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = "poster"
-                )
-            }
+                    .wrapContentSize()
+            )
 
-            Column(
+
+            StyledText(
+                text1 = "Title",
+                text2 = movie.title,
+                color1 = Color.Black,
+                color2 = Color.Gray,
+                modifier = Modifier
+                    .padding(start = 6.dp, top = 12.dp, end = 6.dp)
+                    .constrainAs(titleText) {
+                        start.linkTo(poster.end)
+                        end.linkTo(toggle.start)
+                        width = Dimension.fillToConstraints
+                    }
+            )
+
+            Text(buildAnnotatedString {
+                withStyle(bold) { append("Genre:") }
+                withStyle(gray) { append("${movie.genre}\n") }
+                withStyle(bold) { append("Director:") }
+                withStyle(gray) { append(movie.director) }
+            }, modifier = Modifier
+                .wrapContentHeight()
+                .constrainAs(infoText) {
+                    top.linkTo(titleText.bottom)
+                    start.linkTo(titleText.start)
+                    end.linkTo(parent.end)
+                    width = Dimension.fillToConstraints
+                }
+                .padding(start = 6.dp, bottom = 6.dp, end = 6.dp)
+            )
+
+            Icon(
+                imageVector = if (isVisible.value) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Down Arrow",
+                modifier = Modifier
+                    .padding(end = 4.dp)
+                    .size(25.dp)
+                    .clickable {
+                        isVisible.value = !isVisible.value
+                    }
+                    .constrainAs(toggle) {
+                        end.linkTo(parent.end)
+                    },
+                tint = Color.DarkGray
+            )
+
+            AnimatedVisibility(visible = isVisible.value,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 12.dp)
-            ) {
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(6.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    StyledText(
-                        text1 = "Title",
-                        text2 = movie.title,
-                        color1 = Color.Black,
-                        color2 = Color.Gray
-                    )
-
-                    Icon(
-                        imageVector = if (isVisible.value) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
-                        contentDescription = "Down Arrow",
-                        modifier = Modifier
-                            .padding(end = 4.dp)
-                            .size(25.dp)
-                            .clickable {
-                                isVisible.value = !isVisible.value
-                            },
-                        tint = Color.DarkGray
-                    )
-                }
-
-                Text(buildAnnotatedString {
-                    withStyle(bold) { append("Genre:") }
-                    withStyle(gray) { append("${movie.genre}\n") }
-                    withStyle(bold) { append("Director:") }
-                    withStyle(gray) { append("${movie.director}\n") }
-                }, modifier = Modifier.padding(6.dp))
-
-                AnimatedVisibility(visible = isVisible.value) {
-                    Column {
-                        Text(buildAnnotatedString {
-                            withStyle(bold) { append("Plot:") }
-                            withStyle(gray) { append("${movie.plot}\n") }
-                        })
+                    .padding(4.dp)
+                    .constrainAs(detailText) {
+                        top.linkTo(poster.bottom)
                     }
-                }
+            ) {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(bold) { append("Plot:") }
+                        withStyle(gray) { append(movie.plot) }
+                    }, modifier = Modifier
+                        .wrapContentHeight()
+                        .padding(10.dp)
+                )
             }
         }
     }
