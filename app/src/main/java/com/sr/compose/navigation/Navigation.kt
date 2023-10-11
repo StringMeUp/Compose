d6e8777315fd5af3ce9da7bfe1efa6bf2446bfff
Samir Ramic <ramic.samirr@gmail.com>
2023-10-11 14:21:11 +0200

    [SR-34] Navigate to details using args

--- app/src/main/java/com/sr/compose/navigation/Navigation.kt
+++ app/src/main/java/com/sr/compose/navigation/Navigation.kt
@@ -10,17 +10,18 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.navArgument
-import com.sr.compose.util.NavigationConstants
 import com.sr.compose.R
-import com.sr.compose.util.customSerializable
-import com.sr.compose.ui.screens.bottomnavscreens.MovieScreen
 import com.sr.compose.ui.screens.bottomnavscreens.ContactsScreen
+import com.sr.compose.ui.screens.bottomnavscreens.MovieScreen
 import com.sr.compose.ui.screens.bottomnavscreens.ProfileScreen
 import com.sr.compose.ui.screens.bottomnavscreens.SettingsScreen
+import com.sr.compose.ui.screens.bottomnavscreens.detail.MovieDetailScreen
 import com.sr.compose.ui.screens.main.DefaultArgsScreen
 import com.sr.compose.ui.screens.main.HomeScreen
 import com.sr.compose.ui.screens.main.NullableArgsScreen
 import com.sr.compose.ui.screens.main.SerializableArgsScreen
+import com.sr.compose.util.NavigationConstants
+import com.sr.compose.util.customSerializable
 import com.sr.compose.util.withCustomSerializable
 
 
@@ -36,7 +37,7 @@ fun AppNavigation(
         route = Graph.ROOT
     ) {
         rootArgsGraph(navController, composeItems)
-        bottomNavGraph()
+        bottomNavGraph(navController)
     }
 }
 
@@ -173,13 +174,26 @@ fun NavGraphBuilder.rootArgsGraph(
     }
 }
 
-fun NavGraphBuilder.bottomNavGraph() {
+fun NavGraphBuilder.bottomNavGraph(navController: NavHostController) {
     navigation(
         startDestination = NavigationItem.BottomNavigation.BottomNavMovie.route,
         route = Graph.BOTTOM
     ) {
         composable(route = NavigationItem.BottomNavigation.BottomNavMovie.route) {
-            MovieScreen()
+            MovieScreen(navigateToDetails = { movieId ->
+                navController.navigate(
+                    route = NavigationItem.withRouteArgs(
+                        NavigationItem.BottomNavigation.MovieDetail,
+                        movieId
+                    )
+                )
+
+                /**  Using replace  navController.navigate(
+                route = NavigationItem.BottomNavigation.MovieDetail.route.replace(
+                oldValue = NavigationConstants.Arg_Movie_Detail,
+                newValue = movieId
+                ))*/
+            })
         }
         composable(route = NavigationItem.BottomNavigation.BottomNavProfile.route) {
             ProfileScreen()
@@ -189,6 +203,18 @@ fun NavGraphBuilder.bottomNavGraph() {
         }
         composable(route = NavigationItem.BottomNavigation.BottomNavSettings.route) {
             SettingsScreen()
+        }
+        composable(
+            route = NavigationItem.BottomNavigation.MovieDetail.route,
+            arguments = listOf(navArgument(name = NavigationConstants.Arg_Movie_Detail) {
+                type = NavType.StringType
+                defaultValue = ""
+                nullable = false
+
+            })
+        ) {
+            val arguments = it.arguments?.getString(NavigationConstants.Arg_Movie_Detail)
+            MovieDetailScreen(args = arguments!!)
         }
     }
 }
---
+++ app/src/main/java/com/sr/compose/ui/screens/bottomnavscreens/detail/MovieDetail.kt
@@ -0,0 +1,9 @@
+package com.sr.compose.ui.screens.bottomnavscreens.detail
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MovieDetailScreen(args: String = "") {
+    Text(text = args)
+}
