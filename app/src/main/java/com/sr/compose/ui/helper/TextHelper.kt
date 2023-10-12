9f705f89674ec03dc6b877a1fb56a0cc82f3dd5a
Samir Ramic <ramic.samirr@gmail.com>
2023-10-12 14:32:39 +0200

    [SR-39] Login UI

--- app/src/main/java/com/sr/compose/ui/helper/TextHelper.kt
+++ app/src/main/java/com/sr/compose/ui/helper/TextHelper.kt
@@ -3,9 +3,13 @@ package com.sr.compose.ui.helper
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun StyledText(
@@ -20,3 +24,15 @@ fun StyledText(
         withStyle(style = default) { append(text2) }
     }, modifier = modifier)
 }
+
+val bold = SpanStyle(
+    color = Color.Black,
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Bold,
+    fontFamily = FontFamily.Monospace
+)
+val default = SpanStyle(
+    color = Color.Black,
+    fontWeight = FontWeight.Normal,
+    fontFamily = FontFamily.Monospace
+)
---
+++ app/src/main/java/com/sr/compose/ui/widgets/AppButton.kt
@@ -0,0 +1,37 @@
+package com.sr.compose.ui.widgets
+
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sr.compose.R
+
+@Composable
+@Preview
+fun AppButton(
+    text: String = "",
+    isEnabled: Boolean = true,
+    onClickEvent: () -> Unit = {},
+    modifier: Modifier = Modifier.defaultMinSize(minWidth = 140.dp, minHeight = 48.dp)
+) {
+    OutlinedButton(
+        onClick = { },
+        modifier = modifier,
+        enabled = isEnabled,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = colorResource(id = R.color.s_color),
+            disabledBackgroundColor = Color.Gray,
+            contentColor = colorResource(id = R.color.white)
+
+        )
+    ) {
+        Text(text = text)
+    }
+}
